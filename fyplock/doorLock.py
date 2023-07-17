e0b20f3952af0ad4361ff4c25d8d8b023918b1dd
Timchow <tim.ckchow@gmail.com>
2023-07-17 18:59:08 +0800

    shorten timeout for lock read nfc

--- fyplock/doorLock.py
+++ fyplock/doorLock.py
@@ -99,7 +99,7 @@ class DoorLock:
                                                                     (versiondata >> 16) & 0xFF,
                                                                     (versiondata >> 8) & 0xFF))
         self.nfc.SAMConfig()
-        self.nfc.setPassiveActivationRetries(900)
+        self.nfc.setPassiveActivationRetries(1080)
         print("Starting NFC card detection thread...")
         self.nfc_thread = threading.Thread(target=self.detect_android_nfc_key)
         self.nfc_thread.start()
