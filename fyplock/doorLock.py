44a8b24d75a56d758da368990c29661322183e65
Timchow <tim.ckchow@gmail.com>
2023-07-17 18:03:11 +0800

    testing why android no response in second apdu cmd

--- fyplock/doorLock.py
+++ fyplock/doorLock.py
@@ -123,7 +123,6 @@ class DoorLock:
 
     def authenticate_failed(self, error_message):
         self.nfc.inDataExchange(unlock_failed)
-        print("failed to unlock: " + unlock_failed)
         for i in range(5):
             self.failed_to_unlock = True
             self.error_message = error_message
@@ -148,7 +147,6 @@ class DoorLock:
                         time.sleep(1.1)
                         continue
                     if response == HMAC_SHA256(secret_key, self.random_number):
-                        success, response = self.nfc.inDataExchange(unlock_success)
                         print("unlock: " + unlock_success)
                         self.unlock()
                         return
