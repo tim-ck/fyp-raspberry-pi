d978bf60423984a84c09a1dcec38a36702395276
Timchow <tim.ckchow@gmail.com>
2023-07-21 11:32:21 +0800

    add time sleep before call nfc to prevent no response

--- fyplock/doorLock.py
+++ fyplock/doorLock.py
@@ -102,7 +102,7 @@ class DoorLock:
                                                                     (versiondata >> 16) & 0xFF,
                                                                     (versiondata >> 8) & 0xFF))
         self.nfc.SAMConfig()
-        # self.nfc.setPassiveActivationRetries(80)
+        self.nfc.setPassiveActivationRetries(254)
         print("Starting NFC card detection thread...")
         self.nfc_thread = threading.Thread(target=self.detect_android_nfc_key)
         self.nfc_thread.start()
