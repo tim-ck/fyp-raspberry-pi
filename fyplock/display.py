fbf96eea9f29b68d35f6200fd71987ea0b844630
Timchow <tim.ckchow@gmail.com>
2023-07-21 12:05:02 +0800

    remove display

--- fyplock/display.py
+++ fyplock/display.py
@@ -11,26 +11,26 @@ import subprocess
 class Display:
     def __init__(self, doorLock):
         print("Starting OLED display...")
-        self.doorLock = doorLock
-        self.oled_reset = digitalio.DigitalInOut(board.D4)
-        self.WIDTH = 128
-        self.HEIGHT = 64
-        self.BORDER = 5
-        self.LOOPTIME = 0.2
-        self.i2c = board.I2C()
-        self.oled = adafruit_ssd1306.SSD1306_I2C(self.WIDTH, self.HEIGHT, self.i2c, addr=0x3C, reset=self.oled_reset)
-        self.oled.fill(0)
-        self.oled.show()
-        self.image = Image.new("1", (self.oled.width, self.oled.height))
-        self.draw = ImageDraw.Draw(self.image)
-        self.draw.rectangle((0, 0, self.oled.width, self.oled.height), outline=255, fill=255)
-        self.font = ImageFont.truetype('PixelOperator.ttf', 16)
-        self.draw.rectangle((0, 0, self.oled.width, self.oled.height), outline=0, fill=0)
-        self.draw.text((self.BORDER, self.BORDER), "Door Lock System", font=self.font, fill=255)
-        self.oled.image(self.image)
-        self.oled.show()
-        thread = threading.Thread(target=self.display_loop)
-        thread.start()
+        # self.doorLock = doorLock
+        # self.oled_reset = digitalio.DigitalInOut(board.D4)
+        # self.WIDTH = 128
+        # self.HEIGHT = 64
+        # self.BORDER = 5
+        # self.LOOPTIME = 0.2
+        # self.i2c = board.I2C()
+        # self.oled = adafruit_ssd1306.SSD1306_I2C(self.WIDTH, self.HEIGHT, self.i2c, addr=0x3C, reset=self.oled_reset)
+        # self.oled.fill(0)
+        # self.oled.show()
+        # self.image = Image.new("1", (self.oled.width, self.oled.height))
+        # self.draw = ImageDraw.Draw(self.image)
+        # self.draw.rectangle((0, 0, self.oled.width, self.oled.height), outline=255, fill=255)
+        # self.font = ImageFont.truetype('PixelOperator.ttf', 16)
+        # self.draw.rectangle((0, 0, self.oled.width, self.oled.height), outline=0, fill=0)
+        # self.draw.text((self.BORDER, self.BORDER), "Door Lock System", font=self.font, fill=255)
+        # self.oled.image(self.image)
+        # self.oled.show()
+        # thread = threading.Thread(target=self.display_loop)
+        # thread.start()
     def display_loop(self):
         while True:
             self.draw.rectangle((0, 0, self.oled.width, self.oled.height), outline=0, fill=0)
